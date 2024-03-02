f938776bb29e1f0004abd4430ce8e83637a69ddc
rymkio <148437158+rymkio@users.noreply.github.com>
2024-03-02 17:37:50 +0800

    Update 1_01.cpp

--- chapt1/1_01.cpp
+++ chapt1/1_01.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
+/*
+ * using 和 namespace 都是c++的关键词
+ * 命名空间是在众多名称的可见范围之间竖起的一道道围墙
+ */
 using namespace std;
 
-int main()
-{
+int main() {
     string user_name;
-
-    cout << "Please enter your first name: ";
+    cout << "Please enter your first name:";
     cin >> user_name;
-    cout << "\nHello, " << "Hello,  " << user_name << " ... and goodbye!\n";
-
+    cout << '\n'
+         << "Hello,"
+         << user_name
+         <<"... and goodbye!\n";
     return 0;
 }
